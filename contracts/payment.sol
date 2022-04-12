c11aff43fa572454c3fefec0015f8bdc4ad248f6
kcho2001 <kec623@lehigh.edu>
2022-04-12 15:43:40 -0400

    Added interface to allow access to foreman mappings. Also, added in helper functions to determine how many days a worker has worked

--- contracts/payment.sol
+++ contracts/payment.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.2;
 
 import "./farmFactory.sol";
 
+// Interface to declare functions that will allow us to access foreman mappings
+interface foremanGateway {
+  function getUnpaidWorkDays(address _worker) external view returns (string[] memory);
+  function addUnpaidWorkDay(address _worker, string memory newWorkDay) external;
+  function clearUnpaidWorkDays(address _worker) external;
+  function getWorkHistory(address _worker) external view returns (string[] memory);
+  function setWorkHistory(address _worker, string memory newWorkDay) external;
+}
+
 contract payment is  farmFactory{
-  
+
   event paidWorker(address worker);
 
   // pay a worker a specfic, explicit value
@@ -18,31 +27,31 @@ contract payment is  farmFactory{
 
   // Count up how many times this worker has checked in without getting paid
   // Pay worker for the time that they worked, and update lastDatePaid
-  function queryWorkerDaysUnpaid(address worker) external returns (uint){
+  function queryWorkerDaysUnpaid(address _worker) external view returns (uint){
     uint daysUnpaid;
+    foremanGateway thisForeman;
     //memory array to hold the index of the foreman that the workers had unpaid days with
     for(uint i = 0; i < foremen.length; i++){
       //check to see if this worker has any unpaid days logged with this foreman
       foremanRole fr = foremen[i];
-      daysUnpaid += unpaidWorkDaysWith(fr, worker);
+      thisForeman = foremanGateway(address(fr));
+      daysUnpaid += thisForeman.getUnpaidWorkDays(_worker).length;
     }
     return daysUnpaid;
   }
+
   //function to call when paying a worker
   // maintains the mappings unpaidWorkDays and workHistory
   function adjustWorkerHistory(foremanRole fr, address _worker) internal {
     //adds the information in unpaidWorkDays to workHistory
     //then sets unpaidWorkDays[_worker] = new string[](0);
-    uint size = fr.unpaidWorkDays(_worker).length;
-    for (uint i = 0; i < size; i++){
-      string memory day = fr.unpaidWorkDays(_worker)[i];
-      fr.workHistory(_worker).push(day);
+    foremanGateway thisForeman = foremanGateway(address(fr));
+    string[] memory unpaidDays = thisForeman.getUnpaidWorkDays(_worker);
+    for (uint i = 0; i < unpaidDays.length; i++){
+      string memory day = unpaidDays[i];
+      thisForeman.setWorkHistory(_worker, day);
     }
     //now reset that workers unpaid array
-    fr.unpaidWorkDays[_worker] = new string[](0);
-  }
-  //return unpaid workdays between this foreman and worker
-  function unpaidWorkDaysWith(foremanRole fr, address _worker) internal  returns (uint){
-    return fr.unpaidWorkDays(_worker).length;
+    thisForeman.clearUnpaidWorkDays(_worker);
   }
 }
