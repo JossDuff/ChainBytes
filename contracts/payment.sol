964757b6baa0e6808d7917981a52e279a134a292
kcho2001 <kec623@lehigh.edu>
2022-04-11 22:36:49 -0400

    Started writing payment functionality based on days worked by worker

--- contracts/payment.sol
+++ contracts/payment.sol
@@ -7,12 +7,25 @@ contract payment is farmFactory {
   
   event paidWorker(address worker);
 
-  function payWorker(address worker) external payable{
+  // pay a worker a specfic, explicit value
+  function payWorkerSpecific(address worker) external payable{
     bool sent = payable(worker).send(msg.value);
-    //require sent is true
-
     require(sent, "Worker payment failed");
+    emit paidWorker(worker);
+  }
 
+  // Count up how many times this worker has checked in without getting paid
+  // Pay worker for the time that they worked, and update lastDatePaid
+  function payWorker(address worker) external payable{
+    bytes memory checkIn;
+    uint daysUnpaid;
+    for(uint i = 0; i < foremen.length; i++){
+      if((checkIn = bytes(foremen[i].checkInTime(worker))).length > 1){
+        
+      }
+    }
+    bool sent = payable(worker).send(msg.value);
+    require(sent, "Worker payment failed");
     emit paidWorker(worker);
   }
 }
