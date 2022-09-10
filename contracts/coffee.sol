9c3f27f1f9b0e8d2e823671d17f5678b624fbe6d
joss <jduff360@gmail.com>
2022-09-10 17:35:51 -0400

    Removed some unnecessary getter functions and cleaned up the comments a little.

--- contracts/coffee.sol
+++ contracts/coffee.sol
@@ -26,14 +26,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
     with a region or a name can be handled by the frontend at the time of the farm
     or foreman creation (indicated by the events that are emitted) assuming that 
     these functions are also only being called by the frontend.  Frontend should 
-    also handle querying of relationships because this isn't needed in our MVP.
-
-    TEAM DISCUSSION QUESTION:
-        Why do we need a foreman to be associated with a farm in contract?  
-            As far as I can see, there isn't a need to get the farm when given a foreman in this MVP
-            contract.  Which foremen work at which farm isn't an issue that we're trying to solve
-            irrefutably, so can't this association just be handled by the frontend when an event is emitted?
-        
+    also handle querying of relationships because this isn't needed in our MVP.      
 
 */
 contract coffee is Ownable{
@@ -41,7 +34,7 @@ contract coffee is Ownable{
     event newFarm(address farmAddress);
     event newForeman(address farmAddress, address foreman);
     event workerCheckedIn(address foreman, address worker, string date);
-    event workerPaid(address farm, address worker, uint amount);
+    event workerPaid(address farm, address worker, uint amount, string date);
 
     // Modifier to add to functions that only a farm address can call.
     // Checks isFarm to see if that address is mapped to 'true', indicating
@@ -72,8 +65,11 @@ contract coffee is Ownable{
     mapping(address => bool) isFarm;
 
     // Only the owner (admin) can call this function to create a new farm.
-    // TODO: maybe any address should be able to call this to create their
-    // own farm.
+    // Farms are created by the owner of the contract.  In the real world, this will most likely
+    // be some government agency.
+    // TODO: Who should be owner?  We need a function to transfer ownership of the contract.
+    //       If we go the route of anyone creating a farm, the "check-in" and "pay" functions
+    //       will be basically callable by anyone, and we will have to implement more security.
     function createFarm(address _farmAddress) public onlyOwner {
         // Adds the _farmAddress to the mapping and sets boolean as true,
         // indicating that this address is a farm.
@@ -81,12 +77,6 @@ contract coffee is Ownable{
 
         emit newFarm(_farmAddress);
     }
-
-    // Returns true if the _maybeFarm address is a farm, false otherwise.
-    function isAddressFarm(address _maybeFarm) public view returns(bool){
-        return isFarm[_maybeFarm];
-    }
-
 
     // Mapping for verifying an address is a foreman.
     // Used in onlyForeman modifier.
@@ -104,77 +94,48 @@ contract coffee is Ownable{
         emit newForeman(msg.sender, _foremanAddress);
     }
 
-    // Returns true if address is a foreman, false otherwise.
-    function isAddressForeman(address _maybeForeman) public view returns(bool){
-        return isForeman[_maybeForeman];
-    }
-
-
 /*
-
     Paying workers and checking in.
 
     We have the issue of wanting to be able to keep track of which days the worker
     checked in, and which days they're paid for.  This is difficult and expensive 
-    to do on chain.  I think we can solve this by handling most of it off chain which
-    is okay to do because our goal in this project is simply irrefutably showing that the 
-    worker got paid.  If somone wants to check which days a worker checked in, on the front
-    end we can return a list of all emitted workerCheckedIn events for that address.  
-    This verifier can then compare it to the list of all emitted workerPaid events (also 
-    returned by front end) to makes sure it matches up correctly.
+    to do on chain.  We can solve this by handling payment history and worker/foreman/farm
+    association in a subgraph, which is okay to do because our goal in this project is 
+    simply irrefutably showing that the worker got paid.  If somone wants to check which 
+    days a worker checked in, on the front end we can return a list of all emitted 
+    workerCheckedIn and workerPaid events for that address by querying our subgraph.
 
-    For associating workers to foremen, this can also be handled in the frontend for now because
-    we have no need to get a list of all workers for a foreman yet in this contract.  When the
-    event workerCheckedIn(address foreman, address worker, string date) is called, the frontend
+    For associating workers to foremen, this can also be handled in our subgraph because
+    we have no need to get a list of all workers for a foreman in this contract.  When the
+    event workerCheckedIn(address foreman, address worker, string date) is called, the subgraph
     can add the worker to an array of addresses for a worker.  If someone wanted to verify this,
-    they could pull up a list of all the events emitted by the checkIn function, which logs the 
-    foreman's address who is calling checkIn, the workers address, and the date.
-
+    they could simple query our subgraph for all workers associated with a foreman.
 */
-
-
-    // Mapping for keeping track of the dates that a worker has checked in
-    mapping(address => string[]) daysCheckedIn;
 
     // The farm will call this function with the address of each worker
     // to pay and also the amount of days to pay them for.
-    // Amount of days to pay them for is calculated in frontend.
-    // Update the dates that the worker was paid for in the frontend.
-    // Again, can be independently verified by anyone viewing the history
-    // of the emitted events.
-    function payWorker(address _worker) public payable onlyFarm {
+    // _date is the date this function is getting called.
+    // Update the dates that the worker was paid for in our subgraph.
+    function payWorker(address _worker, string memory _date) public payable onlyFarm {
         // Pays the worker and requires that it was successful,
         // otherwise it failed and the payment doesn't go through
         bool success = payable(_worker).send(msg.value);
         require(success, "Worker payment failed");
 
         // msg.sender is the farm paying the worker
-        emit workerPaid(msg.sender, _worker, msg.value);
+        emit workerPaid(msg.sender, _worker, msg.value, _date);
     }
 
 
     // Foreman calls checkIn with the worker address and the date.  The date is determined by the frontend.
     // Emits the workerCheckedIn event so the frontend can handle sorting which days the worker worked for,
     // and which days the worker is unpaid for.
-    // TODO: what if a foreman forgets to check in workers on a certain date and they want to go back and
-    // check them in later?
-    // TODO: if the foreman can't be trusted with setting the proper date, then we can have a setDate
-    // function that is only callable by the farm, and then in checkIn function we make a call to a 
-    // getDate function to return the date.
     function checkIn(address _workerAddress, string memory _date) public onlyForeman {
-        // We can handle the association between the worker and foreman in the frontend.
+        // We can handle the association between the worker and foreman in the subgraph.
         // This function accomplishes the goal of foremen indicating irrefutably that a worker
         // worked on a particular day as only a foreman can call this function.
-        
-        // Adds the date argument to the string array at the worker's address
-        daysCheckedIn[_workerAddress].push(_date);
 
         emit workerCheckedIn(msg.sender, _workerAddress, _date);
     }
 
-    // Getter function to return the string array in the daysCheckedIn mapping
-    function getDaysCheckedIn(address _workerAddress) public view returns(string[] memory){
-        return daysCheckedIn[_workerAddress];
-    }
-    
 }
