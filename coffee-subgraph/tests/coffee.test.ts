5f6ca5ab5058f5bf5f78717eabd7fc9ba07e704c
Edward Mon <elm224@lehigh.edu>
2023-02-23 15:17:30 -0500

    working testing

--- coffee-subgraph/tests/coffee.test.ts
+++ coffee-subgraph/tests/coffee.test.ts
@@ -7,54 +7,75 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
+// import { ExampleEntity } from "../generated/schema"
 import { OwnershipTransferred } from "../generated/coffee/coffee"
 import { handleOwnershipTransferred } from "../src/coffee"
 import { createOwnershipTransferredEvent } from "./coffee-utils"
+import {Farm } from "../generated/schema"
+import {createnewFarmEvent} from "./coffee-utils"
+import {handlenewFarm} from "../src/coffee"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
-describe("Describe entity assertions", () => {
-  beforeAll(() => {
-    let previousOwner = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let newOwner = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let newOwnershipTransferredEvent = createOwnershipTransferredEvent(
-      previousOwner,
-      newOwner
-    )
-    handleOwnershipTransferred(newOwnershipTransferredEvent)
-  })
 
-  afterAll(() => {
-    clearStore()
-  })
+test ("Creating farm",()=>{
+  let farm = new Farm ("0x0000000000000000000000000000000000000001");
+  farm.save();
+  let address = Address.fromString("0x0000000000000000000000000000000000000002");
 
-  // For more test scenarios, see:
-  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+  let anotherFarm = createnewFarmEvent (address);
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  handlenewFarm (anotherFarm);
 
-    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "previousOwner",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "newOwner",
-      "0x0000000000000000000000000000000000000001"
-    )
+  assert.fieldEquals("Farm", "0x0000000000000000000000000000000000000001", "id","0x0000000000000000000000000000000000000001");
+  assert.fieldEquals("Farm", "0x0000000000000000000000000000000000000002", "id", "0x0000000000000000000000000000000000000002");
+  
 
-    // More assert options:
-    // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
+
+
 })
+
+// describe("Describe entity assertions", () => {
+//   beforeAll(() => {
+//     let previousOwner = Address.fromString(
+//       "0x0000000000000000000000000000000000000001"
+//     )
+//     let newOwner = Address.fromString(
+//       "0x0000000000000000000000000000000000000001"
+//     )
+//     let newOwnershipTransferredEvent = createOwnershipTransferredEvent(
+//       previousOwner,
+//       newOwner
+//     )
+//     handleOwnershipTransferred(newOwnershipTransferredEvent)
+//   })
+
+//   afterAll(() => {
+//     clearStore()
+//   })
+
+//   // For more test scenarios, see:
+//   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+//   test("ExampleEntity created and stored", () => {
+//     assert.entityCount("ExampleEntity", 1)
+
+//     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+//     assert.fieldEquals(
+//       "ExampleEntity",
+//       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+//       "previousOwner",
+//       "0x0000000000000000000000000000000000000001"
+//     )
+//     assert.fieldEquals(
+//       "ExampleEntity",
+//       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+//       "newOwner",
+//       "0x0000000000000000000000000000000000000001"
+//     )
+
+//     // More assert options:
+//     // https://thegraph.com/docs/en/developer/matchstick/#asserts
+//   })
+// })
